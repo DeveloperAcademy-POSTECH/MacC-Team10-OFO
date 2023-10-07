45fda64dc7db5ec70f07b6b00b3f5e3926051de6
Hajime <impapa68@gmail.com>
2023-10-07 22:08:02 +0900

    add: starting files for test SwiftLint

---
+++ Projects/App/Sources/MyApp.swift
@@ -0,0 +1,18 @@
+//
+//  MyApp.swift
+//  App
+//
+//  Created by Ha Jong Myeong on 10/7/23.
+//  Copyright © 2023 com.kozi. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct MyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
