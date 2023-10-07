d40bc81a5c144d8f763dd19b75b79543a41a6746
sozohoy <sozohoy@gmail.com>
2023-10-07 16:10:14 +0900

    Tuist Init

---
+++ Targets/MacCTeam10OFOKit/Tests/MacCTeam10OFOKitTests.swift
@@ -0,0 +1,8 @@
+import Foundation
+import XCTest
+
+final class MacCTeam10OFOKitTests: XCTestCase {
+    func test_example() {
+        XCTAssertEqual("MacCTeam10OFOKit", "MacCTeam10OFOKit")
+    }
+}
---
+++ Targets/MacCTeam10OFOUI/Tests/MacCTeam10OFOUITests.swift
@@ -0,0 +1,8 @@
+import Foundation
+import XCTest
+
+final class MacCTeam10OFOUITests: XCTestCase {
+    func test_example() {
+        XCTAssertEqual("MacCTeam10OFOUI", "MacCTeam10OFOUI")
+    }
+}
