2ef80e4e412197bfaaa1596412a42aa057e1e814
sozohoy <sozohoy@gmail.com>
2023-10-21 00:12:19 +0900

    chore: Feature 모듈 추가(#6)

--- Projects/Feature/Project.swift
+++ Projects/Feature/Project.swift
@@ -5,4 +5,18 @@
 //  Created by 한지석 on 10/20/23.
 //
 
-import Foundation
+import ProjectDescription
+import ProjectDescriptionHelpers
+
+/// watch 개발 시작 후 모듈 구조 변경 예정
+/// Feature -> iOS
+///       -> watchOS
+private let moduleName = "Feature"
+
+let project = Project.makeModule(name: moduleName,
+                                 platform: .iOS,
+                                 product: .staticFramework,
+                                 dependencies: [
+                                    .Project.Common,
+                                    .Project.Core,
+                                 ])
