8e7579608379f89201ab205f5c6a07fc977ab09a
sozohoy <sozohoy@gmail.com>
2023-10-22 01:31:05 +0900

    add: 임시 칼로리 소모, 활동량 레벨(#9)

---
+++ Projects/Core/Sources/Models/CaloriesLevel.swift
@@ -0,0 +1,41 @@
+//
+//  CaloriesLevel.swift
+//  Core
+//
+//  Created by 한지석 on 10/22/23.
+//  Copyright © 2023 com.kozi. All rights reserved.
+//
+
+import Foundation
+
+enum CaloriesLevel: Int {
+    case white
+    case red
+    case orange
+    case yellow
+    case green
+    case blue
+    case indigo
+    case purple
+    
+    var level: Int {
+        switch self {
+        case .white:
+            1
+        case .red:
+            2
+        case .orange:
+            3
+        case .yellow:
+            4
+        case .green:
+            5
+        case .blue:
+            6
+        case .indigo:
+            7
+        case .purple:
+            8
+        }
+    }
+}
