b8efa5d489ca2749acc63f7381e3826a5a134f80
eemdeeks <prince9088@gmail.com>
2023-10-23 04:40:13 +0900

    feat: 사진 추가, 삭제 및 반영 기능추가(#5)

---
+++ Projects/Feature/Sources/ObservedObject/RecordDetailObservedObject.swift
@@ -0,0 +1,90 @@
+//
+//  RecordDetailObservedObject.swift
+//  Feature
+//
+//  Created by 박승찬 on 10/23/23.
+//  Copyright © 2023 com.kozi. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+import PhotosUI
+
+class RecordDetailObservedObject: ObservableObject {
+    @Published var isActionSheetShowing = false
+    @Published var showPicker = false
+
+    @Published var certifyingImage: Image = Image("")
+
+    // MARK: - Image Picker
+
+    enum ImageState {
+        case empty
+        case loading(Progress)
+        case success(Image)
+        case failure(Error)
+    }
+
+    enum TransferError: Error {
+        case importFailed
+    }
+
+    struct CertifyingImage: Transferable {
+        let image: Image
+
+        static var transferRepresentation: some TransferRepresentation {
+            DataRepresentation(importedContentType: .image) { data in
+            #if canImport(AppKit)
+                guard let nsImage = NSImage(data: data) else {
+                    throw TransferError.importFailed
+                }
+                let image = Image(nsImage: nsImage)
+                return ProfileImage(image: image)
+            #elseif canImport(UIKit)
+                guard let uiImage = UIImage(data: data) else {
+                    throw TransferError.importFailed
+                }
+                let image = Image(uiImage: uiImage)
+                return CertifyingImage(image: image)
+            #else
+                throw TransferError.importFailed
+            #endif
+            }
+        }
+    }
+
+    @Published private(set) var imageState: ImageState = .empty
+
+    @Published var imageSelection: PhotosPickerItem? {
+        didSet {
+            if let imageSelection {
+                let progress = loadTransferable(from: imageSelection)
+                imageState = .loading(progress)
+            } else {
+                imageState = .empty
+            }
+        }
+    }
+
+    // MARK: - Private Methods
+
+    private func loadTransferable(from imageSelection: PhotosPickerItem) -> Progress {
+        return imageSelection.loadTransferable(type: CertifyingImage.self) { result in
+            DispatchQueue.main.async {
+                guard imageSelection == self.imageSelection else {
+                    print("Failed to get the selected item.")
+                    return
+                }
+                switch result {
+                case .success(let profileImage?):
+                    self.imageState = .success(profileImage.image)
+                    self.certifyingImage = profileImage.image
+                case .success(nil):
+                    self.imageState = .empty
+                case .failure(let error):
+                    self.imageState = .failure(error)
+                }
+            }
+        }
+    }
+}
--- Projects/Feature/Sources/View/RecordDetailView.swift
+++ Projects/Feature/Sources/View/RecordDetailView.swift
@@ -7,10 +7,11 @@
 //
 
 import SwiftUI
+import PhotosUI
 
 struct RecordDetailView: View {
-    @State var isActionSheetShowing = false
-    @State var certifyingPhoto: String = ""
+    @StateObject var observedObject = RecordDetailObservedObject()
+    @State var showPicker = false
 
     var body: some View {
         ScrollView {
@@ -18,37 +19,90 @@ struct RecordDetailView: View {
                 Text("인증 샷")
                 Spacer()
             }
-            Button(action: {
-                isActionSheetShowing = true
-            }, label: {
+//            Button(action: {
+//                observedObject.isActionSheetShowing = true
+//            }, label: {
+//                Rectangle()
+//                    .fill(Color.gray)
+//                    .frame(height: 500)
+//            })
+//            .padding()
+//            .confirmationDialog("title", isPresented: $observedObject.isActionSheetShowing) {
+//                if observedObject.certifyingPhoto.isEmpty {
+//                    Button {
+//                        observedObject.certifyingPhoto = "사진"
+//                        showPicker.toggle()
+//                    } label: {
+//                        Text("사진 추가")
+//                    }
+//                    PhotosPicker(selection: $observedObject.imageSelection,
+//                                 matching: .images,
+//                                 photoLibrary: .shared()) {
+//                        Text("사진 진짜 추가")
+//                    }
+//                } else {
+//                    Button {
+//                        observedObject.certifyingPhoto = "사진"
+//                    } label: {
+//                        Text("사진 수정")
+//                    }
+//                    Button("사진 삭제", role: .destructive) {
+//                        observedObject.certifyingPhoto = ""
+//                        print("사진 삭제")
+//                    }
+//                }
+//                Button("취소", role: .cancel) {
+//                    print("tap cancel")
+//                }
+//            }
+//            .photosPicker(isPresented: $showPicker, selection: $observedObject.imageSelection)
+
+            CertifyingImageBtn(observedObject: observedObject)
+        }
+    }
+}
+
+struct CertifyingImageBtn: View {
+    @ObservedObject var observedObject: RecordDetailObservedObject
+
+    var body: some View {
+        Button(action: {
+            observedObject.isActionSheetShowing = true
+        }, label: {
+            if observedObject.certifyingImage == Image("") {
                 Rectangle()
                     .fill(Color.gray)
                     .frame(height: 500)
-            })
-            .padding()
-            .confirmationDialog("title", isPresented: $isActionSheetShowing) {
-                if certifyingPhoto.isEmpty {
-                    Button {
-                        certifyingPhoto = "사진"
-                    } label: {
-                        Text("사진 추가")
-                    }
-                } else {
-                    Button {
-                        certifyingPhoto = "사진"
-                    } label: {
-                        Text("사진 수정")
-                    }
-                    Button("사진 삭제", role: .destructive) {
-                        certifyingPhoto = ""
-                        print("사진 삭제")
-                    }
+            } else {
+                observedObject.certifyingImage
+                    .resizable()
+                    .frame(height: 500)
+            }
+        })
+        .padding()
+        .confirmationDialog("title", isPresented: $observedObject.isActionSheetShowing) {
+            if observedObject.certifyingImage == Image("") {
+                Button {
+                    observedObject.showPicker.toggle()
+                } label: {
+                    Text("사진 추가")
                 }
-                Button("취소", role: .cancel) {
-                    print("tap cancel")
+            } else {
+                Button {
+                    observedObject.showPicker.toggle()
+                } label: {
+                    Text("사진 수정")
+                }
+                Button("사진 삭제", role: .destructive) {
+                    observedObject.certifyingImage = Image("")
+                    print("사진 삭제")
                 }
             }
+            Button("취소", role: .cancel) {
+                print("tap cancel")
+            }
         }
+        .photosPicker(isPresented: $observedObject.showPicker, selection: $observedObject.imageSelection)
     }
 }
 
