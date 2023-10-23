23281ed95f27fd92a70422d177b83d02c069cb00
sozohoy <sozohoy@gmail.com>
2023-10-23 21:19:01 +0900

    feat: MainObservableObject Mock 데이터(#18)

--- Projects/Feature/Sources/ObservedObject/MainObservableObject.swift
+++ Projects/Feature/Sources/ObservedObject/MainObservableObject.swift
@@ -1,5 +1,5 @@
 //
-//  MainObservedObject.swift
+//  MainObservableObject.swift
 //  Feature
 //
 //  Created by 한지석 on 10/23/23.
@@ -7,5 +7,20 @@
 //
 
 import Foundation
+import Core
 
-final class MainObservableObject: ObservableObject
+final class MainObservableObject: ObservableObject {
+
+    @Published var player: Player = Player(height: 172,
+                                           weight: 71,
+                                           birth: Date(),
+                                           gender: .male,
+                                           goal: PlayerGoals(goalCalories: 1500,
+                                                             goalDistanceCovered: 4.5),
+                                           statistics: PlayerStatistics(totalDistanceCovered: 46.3,
+                                                                        totalCalories: 16000.2,
+                                                                        totalPlayTime: Date(),
+                                                                        totalPlay: 11),
+                                           average: RecordAverage(record: []))
+
+}
