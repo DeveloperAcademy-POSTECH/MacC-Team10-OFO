8c660accdae47f21a859cc38c4e545917c21cdfd
sozohoy <sozohoy@gmail.com>
2023-10-23 14:22:44 +0900

    add: MainView(#12)

---
+++ Projects/Feature/Sources/View/MainView.swift
@@ -0,0 +1,19 @@
+//
+//  MainView.swift
+//  Feature
+//
+//  Created by 한지석 on 10/23/23.
+//  Copyright © 2023 com.kozi. All rights reserved.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainView()
+}
