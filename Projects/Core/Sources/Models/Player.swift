c7b1218e9a9d559d39244ac03becbef8a4cd3011
sozohoy <sozohoy@gmail.com>
2023-10-23 21:17:00 +0900

    feat: 외부에서 사용 가능하도록 public 생성자 추가(#18)

--- Projects/Core/Sources/Models/Player.swift
+++ Projects/Core/Sources/Models/Player.swift
@@ -16,6 +16,22 @@ public struct Player {
     public let goal: PlayerGoals
     public let statistics: PlayerStatistics
     public let average: RecordAverage
+
+    public init(height: Int,
+                weight: Int,
+                birth: Date,
+                gender: GenderType,
+                goal: PlayerGoals,
+                statistics: PlayerStatistics,
+                average: RecordAverage) {
+        self.height = height
+        self.weight = weight
+        self.birth = birth
+        self.gender = gender
+        self.goal = goal
+        self.statistics = statistics
+        self.average = average
+    }
 }
 
 public struct PlayerStatistics {
@@ -23,11 +39,23 @@ public struct PlayerStatistics {
     public let totalCalories: Double
     public let totalPlayTime: Date
     public let totalPlay: Int
+
+    public init(totalDistanceCovered: Double, totalCalories: Double, totalPlayTime: Date, totalPlay: Int) {
+        self.totalDistanceCovered = totalDistanceCovered
+        self.totalCalories = totalCalories
+        self.totalPlayTime = totalPlayTime
+        self.totalPlay = totalPlay
+    }
 }
 
 public struct PlayerGoals {
     public let goalCalories: Double
     public let goalDistanceCovered: Double
+
+    public init(goalCalories: Double, goalDistanceCovered: Double) {
+        self.goalCalories = goalCalories
+        self.goalDistanceCovered = goalDistanceCovered
+    }
 }
 
 public enum GenderType {
