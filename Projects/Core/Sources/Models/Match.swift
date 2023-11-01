1178dae8018929da04afd3ac83c57a39a8e75601
sozohoy <sozohoy@gmail.com>
2023-11-01 17:02:38 +0900

    feat: Match 데이터 모델 구현(#37)

--- Projects/Core/Sources/Models/Match.swift
+++ Projects/Core/Sources/Models/Match.swift
@@ -6,4 +6,33 @@
 //  Copyright © 2023 com.kozi. All rights reserved.
 //
 
-import Foundation
+import SwiftUI
+import SwiftData
+
+@Model
+public final class Match {
+    public let id: UUID // id == HKStore.id
+    public let matchDate: Date
+    public let distanceCovered: Double
+    public let calories: Double
+    public let playTime: Date
+    public var image: Image?
+    public let goal: PlayerGoals
+
+    public init(
+        id: UUID
+        matchDate: Date,
+        distanceCovered: Double,
+        calories: Double,
+        playTime: Date,
+        image: Image? = nil,
+        goal: PlayerGoals) {
+            self.id = id
+            self.matchDate = matchDate
+            self.distanceCovered = distanceCovered
+            self.calories = calories
+            self.goal = goal
+            self.playTime = playTime
+            self.image = image
+        }
+}
