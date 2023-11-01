9dce4a128d8a88a24d7d9347d4b4ae6e49291547
sozohoy <sozohoy@gmail.com>
2023-11-01 17:02:20 +0900

    feat: Record 데이터 모델 수정. SwiftData 연결 및 데이터 형태 수정(#37)

--- Projects/Core/Sources/Models/Record.swift
+++ Projects/Core/Sources/Models/Record.swift
@@ -6,38 +6,29 @@
 //  Copyright © 2023 com.kozi. All rights reserved.
 //
 
-import SwiftUI
 import Foundation
+import SwiftData
 
-public struct Record {
-    public let matchDate: Date
-    public let distanceCovered: Double
-    public let calories: Double
-    public let goal: PlayerGoals
-    public let playTime: Date
-    public var image: Image?
+// Match와 연관 필요.
+@Model
+public final class Record {
+    public let totalPlay: Int
+    public let totalPlayTime: Date
+    public let totalCalories: Double
+    public let totalDistanceCovered: Double
+    public let matches: [Match]
 
-    public init(matchDate: Date,
-                distanceCovered: Double,
-                calories: Double,
-                goal: PlayerGoals,
-                playTime: Date,
-                image: Image? = nil) {
-        self.matchDate = matchDate
-        self.distanceCovered = distanceCovered
-        self.calories = calories
-        self.goal = goal
-        self.playTime = playTime
-        self.image = image
-    }
-}
-
-public struct RecordAverage {
-    public let record: [Record]
-    public var averageDistanceCovered: Double = 0.8
-    public var averageCalories: Double = 0.47
-
-    public init(record: [Record]) {
-        self.record = record
+    public init(
+        totalPlay: Int,
+        totalPlayTime: Date, 
+        totalCalories: Double,
+        totalDistanceCovered: Double,
+        matches: [Match])
+    {
+        self.totalPlay = totalPlay
+        self.totalPlayTime = totalPlayTime
+        self.totalCalories = totalCalories
+        self.totalDistanceCovered = totalDistanceCovered
+        self.matches = matches
     }
 }
